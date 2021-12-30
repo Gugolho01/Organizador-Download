73aec2e626b6e4d431a54c3817631bbd4cc2e0c4
Gustavo Lima dos Santos <92238035+Gugolho01@users.noreply.github.com>
2021-12-30 11:33:47 -0300

    Update and rename Organizador Download to Main.py

---
+++ Main.py
@@ -0,0 +1,11 @@
+import Bibli
+
+Bibli.orgArq('*.abr', '*.cm', '*.arquivo')
+Bibli.orgAud('*.mp3')
+Bibli.orgAdo('*.Adobe photoshop image.20', '*.psd')
+Bibli.orgImg('*.jpeg', '*.jpg', '*.png', '*.gif', '*.jfif', '*.raw', '*.bmp')
+Bibli.orgVid('*.mp4', '*.mov', '*.avi', '*.flv')
+Bibli.orgRaR('*.winrar', '*.zip', '*.rar')
+Bibli.orgTor('*.torrent')
+Bibli.orgApp('*.exe', '*.msi')
+Bibli.orgCod('py', 'cs', 'js', 'php', 'html', 'sql', 'css')
